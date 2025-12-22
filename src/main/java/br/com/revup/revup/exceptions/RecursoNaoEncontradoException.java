d2d34726181d2d38a4277e82f075056243fcae86
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-12-22 15:03:12 -0300

    feat(#7): RecursoNaoEncontradoException

---
+++ src/main/java/br/com/revup/revup/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,9 @@
+package br.com.revup.revup.exceptions;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+    
+}
