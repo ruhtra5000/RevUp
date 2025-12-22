03a661f1563a6f62a2fde7732e4d36d3d5ee9551
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-12-22 10:52:53 -0300

    commit inicial

---
+++ src/main/java/br/com/revup/revup/RevupApplication.java
@@ -0,0 +1,13 @@
+package br.com.revup.revup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevupApplication.class, args);
+	}
+
+}
