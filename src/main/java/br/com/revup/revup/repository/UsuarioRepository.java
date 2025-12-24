6974cdbaf7ab8a47d9ae677cbd402543e40628ca
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-12-24 15:12:15 -0300

    feat(#4): findByEmail em UsuarioRepository

--- src/main/java/br/com/revup/revup/repository/UsuarioRepository.java
+++ src/main/java/br/com/revup/revup/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.revup.revup.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -17,6 +19,8 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>,
                                            QuerydslPredicateExecutor<Usuario>,
                                            QuerydslBinderCustomizer<QUsuario>
 {
+	Optional<Usuario> findByEmail(String email);
+    
     @Override
     default void customize(QuerydslBindings bindings, QUsuario root) {
         // Bind para ID
