74e34b2cde1c343658c9ca844e5953ba639561ac
Arthur de Sá Tenório <arthursa147@gmail.com>
2026-01-07 23:27:06 -0300

    feat(#1, #9): construtores de Imagem

--- src/main/java/br/com/revup/revup/entity/Imagem.java
+++ src/main/java/br/com/revup/revup/entity/Imagem.java
@@ -4,14 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Imagem {
+    // Atributos
     @Id
     @GeneratedValue
     private long id;
     private String path;        // Caminho do arquivo 
     private String nome;        // Nome do arquivo 
     private String tipoArquivo; // Extensão do arquivo (png, jpeg, ...)
+
+    // Construtor
+    public Imagem (String path, String nome, String tipoArquivo) {
+        this.path = path;
+        this.nome = nome;
+        this.tipoArquivo = tipoArquivo;
+    }
 }
